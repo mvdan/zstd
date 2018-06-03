ad012cbc9d2eb470eeaeb9f2dfb8db9ebf4c3b2d
Daniel Martí <mvdan@mvdan.cc>
2018-06-03 14:58:57 +0100

    wuffsmain: rewrite after examples/zcat/zcat.c
    It's a newer example, so reusing bits from it brings certain advantages
    such as being stream-based and no longer limited to a certain amount of
    bytes of input and output.
    
    Fixes #3.

--- wuffsmain.c
+++ wuffsmain.c
@@ -1,72 +1,75 @@
 #include <errno.h>
-#include <stdlib.h>
 #include <unistd.h>
 
 #include "decode.c"
 
-#define SRC_BUF_SIZE (64 * 1024 * 1024)
-#define DST_BUF_SIZE (64 * 1024 * 1024)
+#ifndef DST_BUFFER_SIZE
+#define DST_BUFFER_SIZE (16 * 1024)
+#endif
 
-uint8_t src_buf[SRC_BUF_SIZE] = {0};
-size_t src_len = 0;
-uint8_t dst_buf[DST_BUF_SIZE] = {0};
-size_t dst_len = 0;
+#ifndef SRC_BUFFER_SIZE
+#define SRC_BUFFER_SIZE (16 * 1024)
+#endif
 
-uint8_t* print_buf = NULL;
-size_t print_len = 0;
+char dst_buffer[DST_BUFFER_SIZE];
+char src_buffer[SRC_BUFFER_SIZE];
 
 static void ignore_return_value(int ignored) {}
 
-const char* read_stdin() {
-	while (src_len < SRC_BUF_SIZE) {
-		const int stdin_fd = 0;
-		ssize_t n = read(stdin_fd, src_buf + src_len, SRC_BUF_SIZE - src_len);
-		if (n > 0) {
-			src_len += n;
-		} else if (n == 0) {
-			return NULL;
-		} else if (errno == EINTR) {
-			// no-op
-		} else {
-			return strerror(errno);
-		}
-	}
-	return "input is too large";
-}
-
-const char* decode() {
-	wuffs_base__io_buffer src = {.ptr = src_buf, .len = src_len, .wi = src_len, .closed = true};
-	wuffs_base__io_reader src_reader = wuffs_base__io_buffer__reader(&src);
-
-	wuffs_base__io_buffer dst = {.ptr = dst_buf, .len = DST_BUF_SIZE};
-	wuffs_base__io_writer dst_writer = wuffs_base__io_buffer__writer(&dst);
-
+static const char* decode() {
 	wuffs_zstd__decoder dec = ((wuffs_zstd__decoder){});
 	wuffs_zstd__decoder__check_wuffs_version(&dec, sizeof dec, WUFFS_VERSION);
 
-	wuffs_zstd__status s = wuffs_zstd__decoder__decode(&dec, dst_writer, src_reader);
-	if (s) {
-		return wuffs_zstd__status__string(s);
+	while (true) {
+		const int stdin_fd = 0;
+		ssize_t n_src = read(stdin_fd, src_buffer, SRC_BUFFER_SIZE);
+		if (n_src < 0) {
+			if (errno != EINTR) {
+				return strerror(errno);
+			}
+			continue;
+		}
+
+		wuffs_base__io_buffer src = ((wuffs_base__io_buffer){
+				.ptr = src_buffer,
+				.len = SRC_BUFFER_SIZE,
+				.wi = n_src,
+				.closed = n_src == 0,
+		});
+		wuffs_base__io_reader src_reader = wuffs_base__io_buffer__reader(&src);
+
+		while (true) {
+			wuffs_base__io_buffer dst = ((wuffs_base__io_buffer){.ptr = dst_buffer, .len = DST_BUFFER_SIZE});
+			wuffs_base__io_writer dst_writer = wuffs_base__io_buffer__writer(&dst);
+			wuffs_zstd__status s = wuffs_zstd__decoder__decode(&dec, dst_writer, src_reader);
+
+			if (dst.wi) {
+				const int stdout_fd = 1;
+				ignore_return_value(write(stdout_fd, dst_buffer, dst.wi));
+			}
+
+			if (s == WUFFS_ZSTD__STATUS_OK) {
+				return NULL;
+			}
+			if (s == WUFFS_ZSTD__SUSPENSION_SHORT_READ) {
+				break;
+			}
+			if (s != WUFFS_ZSTD__SUSPENSION_SHORT_WRITE) {
+				return wuffs_zstd__status__string(s);
+			}
+		}
 	}
-	ignore_return_value(write(1, dst.ptr, dst.wi));
-	return NULL;
 }
 
 int fail(const char* msg) {
 	const int stderr_fd = 2;
-	write(stderr_fd, msg, strnlen(msg, 4095));
-	write(stderr_fd, "\n", 1);
+	ignore_return_value(write(stderr_fd, msg, strnlen(msg, 4095)));
+	ignore_return_value(write(stderr_fd, "\n", 1));
 	return 1;
 }
 
 int main(int argc, char** argv) {
-	const char* msg = read_stdin();
-	if (msg) {
-		return fail(msg);
-	}
-	msg = decode();
-	if (msg) {
-		return fail(msg);
-	}
-	return 0;
+	const char* msg = decode();
+	int status = msg ? fail(msg) : 0;
+	return status;
 }
